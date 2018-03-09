1d2860970ac86aab265f091147e7c6361161648b
Felipe Fragoso <felipe.fragoso@hotmail.com>
2018-03-09 14:27:38 -0300

    Preparando interface pro OAuth2

--- src/app/login/content/login-card.component.ts
+++ src/app/login/content/login-card.component.ts
@@ -1,40 +1,52 @@
-import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
-import { Router } from '@angular/router';
+import {Component, OnInit, Output, EventEmitter, Input} from '@angular/core';
+import {Router} from '@angular/router';
 
-import { LoginService }	from '../login.service';
-import { InComponent } from "@app/model";
-import { NotificationsService } from "angular2-notifications";
-import { MatDialog } from "@angular/material";
-import { DialogResetPasswordComponent } from "./dialog-reset-password.component";
-import { UsuarioService } from "../../content/usuario/usuario.service";
+import {LoginService} from '../login.service';
+import {InComponent} from "@app/model";
+import {NotificationsService} from "angular2-notifications";
+import {MatDialog} from "@angular/material";
+import {DialogResetPasswordComponent} from "./dialog-reset-password.component";
+import {UsuarioService} from "../../content/usuario/usuario.service";
 
 @Component({
-	selector: 'login-card',
-	templateUrl: 'login-card.component.html',
+    selector: 'login-card',
+    templateUrl: 'login-card.component.html',
 })
 export class LoginCardComponent extends InComponent implements OnInit {
 
-	public email:string;
-	public senha:string;
+    public email: string;
+    public senha: string;
 
-	@Output() loginCompleted = new EventEmitter();
+    @Output() loginCompleted = new EventEmitter();
 
-	public visitorEmail:string;
+    public visitorEmail: string;
 
-	constructor(private loginService:LoginService, private usuarioService:UsuarioService, private router:Router, private notification:NotificationsService, public dialog: MatDialog) {
-		super();
-	}
+    constructor(private loginService: LoginService, private usuarioService: UsuarioService, private router: Router, private notification: NotificationsService, public dialog: MatDialog) {
+        super();
+    }
 
-	ngOnInit(): void {
-	}
+    ngOnInit(): void {
+    }
 
-	private validateLogin():boolean {
-		if(!this.email || !this.senha)
-			return false;
+    private validateLogin(): boolean {
+        if (!this.email || !this.senha)
+            return false;
 
-		if(this.email.length < 5 || this.senha.length < 5)
-			return false;
+        if (this.email.length < 5 || this.senha.length < 5)
+            return false;
 
-		return true;
-	}
+        return true;
+    }
+
+    public realizarLogin() {
+        this.loginService.fazerLogin(this.email, this.senha).subscribe(
+            result => {
+                this.notification.success("MaisVida", "Login realizado com sucesso.");
+            },
+            error => {
+                this.notification.error("MaisVida", "Não foi possível realizar o login, tente novamente mais tarde.")
+            }
+        )
+    }
+
 }
--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,13 +1,40 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
-import { AppService }	from '../app.service';
+import {AppService} from '../app.service';
 
-import { Observable } from 'rxjs/Observable';
-import { MaisVidaResponse } from "@app/model";
+import {Observable} from 'rxjs/Observable';
+import {MaisVidaResponse, Medico} from "@app/model";
+import {catchError, map} from "rxjs/operators";
+
 
 @Injectable()
 export class LoginService extends AppService {
 
-	constructor () { super(); }
+    constructor(private http: HttpClient) {
+        super();
+    }
 
+    verificaLoginAutenticado(): boolean {
+        if (localStorage.getItem("accessToken"))
+            return true;
+
+        return false;
+    }
+
+    fazerLogin(usuario: string, senha: string, lembrarSenha: boolean = false): Observable<any> {
+        let formData: FormData = new FormData();
+        formData.append('username', usuario);
+        formData.append('password', senha);
+
+        if (lembrarSenha) {
+            localStorage.setItem("usuario", usuario);
+            localStorage.setItem("senha", senha);
+        }
+
+        return this.http.post<any>(this.baseApi + "oauth/token", formData, this.getHeaders()).pipe(
+            map(this.extractData),
+            catchError(this.handleError)
+        )
+    }
 }
